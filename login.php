a2fcd0830f94522cdadab950b9e9fa88934764f0
adrianN4m1 <ralph12mones@gmail.com>
2024-04-12 18:30:53 +0800

    login

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Starubigaz_Admin/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="Starubigaz_Admin/assets/css/-Login-form-Page-BS4--Login-form-Page-BS4.css">
     <link rel="stylesheet" href="Starubigaz_Admin/assets/css/Add-Another-Button-1.css">
@@ -20,7 +20,7 @@
             <div class="card-body p-0">
                 <div class="row">
                     <div class="col-lg-6 d-none d-lg-flex">
-                        <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/login_img.jpg&quot;);"></div>
+                        <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;Starubigaz_Admin/assets/img/login_img.jpg&quot;);"></div>
                     </div>
                     <div class="col-lg-6">
                         <div class="p-5">
